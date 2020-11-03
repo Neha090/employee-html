046fadca638822abfebdd90685c9cbfa353f0cb5
neha <nehasaini0130@gmail.com>
2020-11-03 18:34:47 +0530

    commiting dev-v1

--- emp.js
+++ emp.js
@@ -1,82 +1,60 @@
 var arr = [];
-
-function Employee(empId, name, dept) {
-    this.empId = empId;
-    this.name = name;
-    this.dept = dept;
-}
-var emp = new Employee();
-
-Employee.prototype.addEmp = function () {
-    arr = emp.getEmployeesFromString(localStorage.getItem("employee"));
-    var employee = { empId: this.empId, name: this.name, dept: this.dept };
-    arr.push(employee);
-    localStorage.setItem("employee", JSON.stringify(arr));
-}
-
-Employee.prototype.readEmp = function () {
-
-    var res = emp.getEmployeesFromString(localStorage.getItem("employee"));
-    stringEmp = "<tr><th> EmpID</th><th>Name</th><th>Department</th><th>Actions</th></tr >";
-    for (let i = 0; i < res.length; i++) {
-        stringEmp += "<tr><td>" + res[i].empId + "</td>" +
-            "<td>" + res[i].name + "</td>" +
-            "<td>" + res[i].dept +
-            "</td> <td><span id='clickableAwesomeFont'> <i class='fa fa-pencil-square-o' onclick='updateEmployee(" + res[i].empId + ")'aria-hidden='true'></i></span>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;" +
-            "<span id='clickableAwesomeFont'><i class='fa fa-trash'aria-hidden='true' onclick='deleteEmp(" + res[i].empId + ")'></i></span></td></tr>";
-    }
-    document.getElementById("result").innerHTML = stringEmp;
-}
-
-Employee.prototype.getEmployeesFromString = function (str) {
+function getEmployeesFromString(str) {
     try {
         if (!str) {
             return [];
         }
-        console.log(str);
         return JSON.parse(str);
     } catch (e) {
         return [];
     }
 }
 
-Employee.prototype.delEmp = function () {
-    arr = emp.getEmployeesFromString(localStorage.getItem("employee"));
-    var index = arr.findIndex((element) => { return element.empId == empId });
-    arr.splice(index, 1);
-    localStorage.setItem("employee", JSON.stringify(arr));
-    emp.readEmp();
-}
-
-function addEmployee(empId) {
-    console.log("add called");
+function addEmp(empId = null, name = null, dept = null) {
+    arr = getEmployeesFromString(localStorage.getItem("employee"));
     if (empId != null) {
-        show();
         document.forms["emp"]["empId"].value = empId;
+        document.forms["emp"]["name"].value = name;
+        document.forms["emp"]["dept"].value = dept;
         document.getElementById("empId").disabled = true;
-        emp.delEmp(empId)
+        deleteEmp(empId);
         return;
     }
-    else if(empId==null){
-    empId = document.forms["emp"]["empId"].value;
-    name = document.forms["emp"]["name"].value;
-    dept = document.forms["emp"]["dept"].value;
+    else if(empId == null) {
+        empId = document.forms["emp"]["empId"].value;
+        name = document.forms["emp"]["name"].value;
+        dept = document.forms["emp"]["dept"].value;
     }
-    var emp1 = new Employee(empId, name, dept);
-    emp1.addEmp();
-    emp1.read();
-    return false;
+
+    var emp = { "empId": empId, "name": name, "dept": dept };
+
+    arr.push(emp);
+    localStorage.setItem("employee", JSON.stringify(arr));
+    readEmp();
 }
 
-function deleteEmp(empId) {
-    emp.delEmp(empId);
+function readEmp() {
+    var res = getEmployeesFromString(localStorage.getItem("employee"));
+    stringEmp = "<tr><th> EmpID</th><th>Name</th><th>Department</th><th>Actions</th></tr >";
+    for (let i = 0; i < res.length; i++) {
+        stringEmp += "<tr><td>" + res[i].empId + "</td>" +
+            "<td>" + res[i].name + "</td>" +
+            "<td>" + res[i].dept +
+            "</td> <td><span id='clickableAwesomeFont'> <i class='fa fa-pencil-square-o' onclick='updateEmp(" + res[i].empId + ")'aria-hidden='true'></i></span>" +
+            "&nbsp;&nbsp;&nbsp;&nbsp;" +
+            "<span id='clickableAwesomeFont'><i class='fa fa-trash'aria-hidden='true' onclick='deleteEmp(" + res[i].empId + ")'></i></span></td></tr>";
+    }
+
+    document.getElementById("result").innerHTML = stringEmp;
+    window.onload = readEmp;
 }
 
-function updateEmployee(empId) {
-    alert("Kindly make the updates as required");
-    addEmployee(empId);
-}
+window.onload = readEmp;
+//var ever = setInterval(readEmp, 10000);
+
+window.addEventListener("storage", function () {
+    readEmp();
+}, false);
 
 function show() {
     var x = document.getElementById("empForm");
@@ -86,4 +64,26 @@ function show() {
     }
 }
 
-window.onload = emp.readEmp;
+function deleteEmp(empId) {
+    console.log("delete");
+    arr = getEmployeesFromString(localStorage.getItem("employee"));
+    var index = arr.findIndex((element) => { return element.empId == empId });
+    arr.splice(index, 1);
+    console.log(arr);
+    localStorage.removeItem("employee");
+    localStorage.setItem("employee", JSON.stringify(arr));
+    readEmp();
+    console.log(arr);
+
+}
+
+function updateEmp(empId) {
+    console.log("update");
+    arr = getEmployeesFromString(localStorage.getItem("employee"));
+    var index = arr.findIndex((element) => { return element.empId == empId });
+    alert("Kindly make the updates as required");
+    show();
+    addEmp(empId, arr[index].name, arr[index].dept);
+
+
+}
